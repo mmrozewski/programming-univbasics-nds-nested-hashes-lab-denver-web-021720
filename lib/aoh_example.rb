018626bff4883c24014460743e06d7fbb9daa1d9
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-07 22:21:42 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,8 +11,9 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  [{name: "Don Gately", occupation: "Live-in Staff"},
+   {name: "Joelle van Dyne", occupation: "Radio Personality"},
+   {name:  "Pat Monteseian", occupation: "Staff" },]
 end
 
 def aoh_lookup(aoh, row, key)
